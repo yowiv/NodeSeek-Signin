1058f28af8f41180ea1fb0aae7339d491e2ec7a1
YYWO <112981137+YYWO@users.noreply.github.com>
2024-05-27 22:14:02 +0800

    更新青龙面板变量

--- nodeseek_sign.py
+++ nodeseek_sign.py
@@ -1,9 +1,13 @@
 # -- coding: utf-8 --
 import requests
 import os
-
+import sys
 random = "false"  # 随机签到1-x鸡腿为true，固定鸡腿*5为false
-Cookie = os.environ.get("COOKIE","")
+
+NS_COOKIE = os.environ.get("NS_COOKIE","")
+COOKIE = os.environ.get("COOKIE", "")
+COOKIE_ENV = NS_COOKIE or COOKIE
+
 pushplus_token = os.environ.get("PUSHPLUS_TOKEN")
 telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN","")
 chat_id = os.environ.get("CHAT_ID","")
@@ -29,7 +33,24 @@ def pushplus_ts(token, rw, msg):
     msg = r.json().get('msg', None)
     print(f'pushplus推送结果：{msg}\n')
 
-if Cookie:
+def load_send():
+    global send
+    global hadsend
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(cur_path)
+    if os.path.exists(cur_path + "/notify.py"):
+        try:
+            from notify import send
+            hadsend=True
+        except:
+            print("加载notify.py的通知服务失败，请检查~")
+            hadsend=False
+    else:
+        print("加载通知服务失败,缺少notify.py文件")
+        hadsend=False
+load_send()
+
+if COOKIE_ENV:
     url = f"https://www.nodeseek.com/api/attendance?random={random}"
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
@@ -42,16 +63,17 @@ if Cookie:
         'sec-fetch-dest': "empty",
         'referer': "https://www.nodeseek.com/board",
         'accept-language': "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        'Cookie': Cookie
+        'Cookie': COOKIE_ENV
     }
 
     try:
         response = requests.post(url, headers=headers,verify=False)
         response_data = response.json()
         print(response_data)
+        print(COOKIE_ENV)
         message = response_data.get('message')
         success = response_data.get('success')
-        
+        send("nodeseek签到", message)
         if success == "true":
             print(message)
             if telegram_bot_token and chat_id:
