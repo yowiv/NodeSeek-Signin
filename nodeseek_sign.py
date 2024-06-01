6d518bbd9f2dcf25180dd9d3aad7bb71bc242bb7
Chen-Li-liang <chen568244044@gmail.com>
2024-06-02 07:30:01 +0800

    Update nodeseek_sign.py

--- nodeseek_sign.py
+++ nodeseek_sign.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from curl_cffi import requests
-random = "false"  # 随机签到1-x鸡腿为true，固定鸡腿*5为false
+random = "true"  # 随机签到1-x鸡腿为true，固定鸡腿*5为false
 
 NS_COOKIE = os.environ.get("NS_COOKIE","")
 COOKIE = os.environ.get("COOKIE", "")
